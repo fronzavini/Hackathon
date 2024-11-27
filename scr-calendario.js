915f7f4f8346eb78c17711476514713a354e69cd
Antônio Henrique Rohling Froehner <rf.antoniohenrique@gmail.com>
2024-11-27 19:44:45 -0300

    Add files via upload

---
+++ scr-calendario.js
@@ -0,0 +1,61 @@
+const calendar = document.getElementById('calendar');
+const today = new Date();
+const currentMonth = today.getMonth();
+const currentYear = today.getFullYear();
+const currentDay= today.getDate();
+
+function createCalendar(month, year){
+    const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const totalDays = new Date(year, month + 1,0).getDate();
+    const firstDayIndex = new Date(year, month, 1).getDay();
+
+let html = `<section id="menu-calendario"><h2 id="month-year">${monthNames[month]} ${year}</h2>`;
+    html += `<div id="botao-mes"><button type="button" id="b1"></button><button type="button" id="b2"></button></div></section>`;
+    html += `<table>`;
+    html += `<tr><th>Dom</th><th>Seg</th><th>Ter</th><th>Qua</th><th>Qui</th><th>Sex</th><th>Sab</th></tr>`
+    
+    let day = 1;
+    for(let i=0; i < 6; i++) {
+        html += `<tr>`;
+        for(let j = 0; j < 7; j++){
+            const classNames = [];
+            if (i === 0 && j < firstDayIndex) {
+                html += `<td></td>`;
+            }else if (day > totalDays){
+                break;
+            }else {
+                const date = `${year}-${String(month + 1).padStart(2, '0')}-${String(day).padStart(2, '0')}`;
+                html += `<td class="calendar-day" data-date="${date}">${day}</td>`;
+                //html += `<td class="${classNames.join('')}">${day}</td>`;
+                day++;
+            }
+        }
+        html += `</tr>`;
+    }
+    html += `</table>`;
+
+    calendar.innerHTML = html;
+
+    document.querySelectorAll('.calendar-day').forEach(day => {
+        day.addEventListener('click', function () {
+            const date = this.getAttribute('data-date');
+            // Redireciona para a página desejada
+            window.location.href = `horarios-aluno.html`;
+        });
+    });
+}
+
+createCalendar(currentMonth, currentYear);
+/*
+document.addEventListener('mouseover', function(event){
+    if (event.target.tagName=== 'TD'){
+    event.target.classList.add('highlight');
+    }
+});
+
+document.addEventListener('mouseout', function(event){
+    if (event.target.tagName==='TD') {
+    event.target.classList.remove('highlight');
+    }
+});
+*/
---
+++ scr-horario-tabela.js
@@ -0,0 +1,46 @@
+const horario = document.getElementById('horario');
+const horas = ["08:00", "08:45", "09:45", "10:30", "11:15", "", "13:30", "14:15", "15:00", "16:00", "16:45"];
+
+function criarHorario(){
+    
+let html = `<section id="menu-calendario"><h2 id="month-year">
+    <select name="Labs" id="labs">
+        <option value="LabA03" class="select">Lab A03</option>
+        <option value="LabA04" class="select">Lab A04</option>
+    </select>
+    </h2>`;
+    html += `<div id="botao-mes"><button type="button" id="b1"></button><button type="button" id="b2"></button></div></section>`;
+    html += `<table>`;
+html += `<tr><th></th><th>Seg</th><th>Ter</th><th>Qua</th><th>Qui</th><th>Sex</th><th>Sab</th></tr>`;
+
+for(let i=0; i < 11; i++) {
+    html += `<tr>`;
+    for(let j = 0; j < 7; j++){
+        if(j === 0){
+            html += `<td class="horario-celula" id="hora">${horas[i]}</td>`;
+        }
+        else{
+            html += `<td class="horario-celula"></td>`;
+        }
+    }
+    html += `</tr>`;
+}
+html += `</table>`;
+
+horario.innerHTML = html;
+}
+
+criarHorario();
+/*
+document.addEventListener('mouseover', function(event){
+    if (event.target.tagName=== 'TD'){
+    event.target.classList.add('highlight');
+    }
+});
+
+document.addEventListener('mouseout', function(event){
+    if (event.target.tagName==='TD') {
+    event.target.classList.remove('highlight');
+    }
+});
+*/
